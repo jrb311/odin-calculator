1b93ed10f5f20adb6bc0d6316ea095009e9f1e9d
jrb311 <159147700+jrb311@users.noreply.github.com>
2024-04-13 11:37:24 -0400

    Add generateDisplay function
    Included eventlisteners for digit buttons and operator buttons, which
    populate the display on click, store firstnum values, store op value,
    resets display on clicking digit btn while op has value, and generates
    display with new value to be stored in secondNum.

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 let firstNum;
 let secondNum;
 let operator;
+let display = document.querySelector("#display");
+display.textContent = "0";
+
+generateDisplay();
+
+
+
+function generateDisplay() {
+    //Generate numbers in display on user click
+    let digits = document.querySelectorAll(".digit-button");
+    digits.forEach(digit => {
+        digit.addEventListener('click', (event) => {
+            display.textContent = 
+                operator === undefined ? parseInt(display.textContent.concat(event.target.textContent))
+                : display.textContent !== firstNum ? parseInt(display.textContent.concat(event.target.textContent))
+                    : (() => {
+                        display.textContent = 0;
+                        return parseInt(display.textContent.concat(event.target.textContent)) 
+                    }) ();
+        });
+    });
+    //Store first number value and operator value on user click for use in calulator functions
+    let operatorNode = document.querySelectorAll(".op-button");
+    operatorNode.forEach(op => {
+        op.addEventListener('click', (event) => {
+            firstNum = display.textContent;
+            operator = event.target.textContent;
+            
+        });
+    });
+
+     
+};
 
 function add(a, b) {
     return a + b;
